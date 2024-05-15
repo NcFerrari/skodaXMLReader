757923291f3185aa9fd33bfe18097966eb65b91a
Lubor Pešek <lubor.pesek@qinshift.com>
2024-05-15 12:53:31 +0200

    sonar refactoring

--- src/main/java/lp/fe/App.java
+++ src/main/java/lp/fe/App.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
         Group group = new Group();
         Scene scene = new Scene(group, 800, 500);
         stage.setScene(scene);
